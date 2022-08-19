1e024b0c3d2bbf51313d19a49f21ddf14e060d15
Darek <darek.bidek@gmail.com>
2022-08-19 15:33:34 -0400

    Removing version number to pull the latest

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
 export {Application, Router, send} from 'https://deno.land/x/oak/mod.ts';
 
 // @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
-import React from "https://jspm.dev/react@18.1.0";
+import React from "https://jspm.dev/react";
 
 // @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts"
-import ReactDOMServer from "https://jspm.dev/react-dom@18.1.0/server";
+import ReactDOMServer from "https://jspm.dev/react-dom/server";
 
 export { React, ReactDOMServer };
